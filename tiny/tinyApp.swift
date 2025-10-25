8c86e7af144d21739c9dc27a218d916098c09c99
destucr <destucr@gmail.com>
2025-10-25 18:48:43 +0700

    add tiny project

---
+++ tiny/tinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinyApp.swift
+//  tiny
+//
+//  Created by Destu Cikal Ramdani on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct tinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
