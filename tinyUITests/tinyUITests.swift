037af1d5e84b58b6372a3eaa2f44cd614e50a530
destucr <destucr@gmail.com>
2025-10-27 20:13:15 +0700

    add swiftlint script

--- tinyUITests/tinyUITests.swift
+++ tinyUITests/tinyUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class tinyUITests: XCTestCase {
+final class TinyUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
