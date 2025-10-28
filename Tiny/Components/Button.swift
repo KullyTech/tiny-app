103455b27fadab2865f509d22a17b37e9648d60d
destucr <destucr@gmail.com>
2025-10-28 23:35:41 +0700

    update: add button with gradient background

---
+++ Tiny/Components/Button.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Tiny
+//
+//  Created by Destu Cikal Ramdani on 28/10/25.
+//
+
+import SwiftUI
+
+struct Button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Button()
+}
