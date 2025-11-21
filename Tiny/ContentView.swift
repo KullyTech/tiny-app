10d58fe118ac3f3c433d4f945e6e6d29723545dc
Revanza Narendra <revanzanrndra@gmail.com>
2025-11-21 21:59:56 +0700

    fix: save recording to local

--- Tiny/ContentView.swift
+++ Tiny/ContentView.swift
@@ -6,22 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @AppStorage("hasShownOnboarding") var hasShownOnboarding: Bool = false
+    @StateObject private var heartbeatSoundManager = HeartbeatSoundManager()
+    @State private var showTimeline = false
+    
+    @Environment(\.modelContext) private var modelContext
 
     var body: some View {
         Group {
             if hasShownOnboarding {
-                OrbLiveListenView()
+                HeartbeatMainView()
             } else {
                 OnBoardingView(hasShownOnboarding: $hasShownOnboarding)
             }
         }
         .preferredColorScheme(.dark)
+        .onAppear {
+            heartbeatSoundManager.modelContext = modelContext
+            heartbeatSoundManager.loadFromSwiftData()
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: SavedHeartbeat.self, inMemory: true)
 }
